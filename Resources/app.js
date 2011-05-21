be26172795b46e297e20e9c64ea3da1e8ef6ce69
marcus.ross@zahlenhelfer.de <marcusross@Mangold.local>
2011-05-21 17:19:13 +0200

    Dokumentation erweitert

--- Resources/app.js
+++ Resources/app.js
@@ -3,28 +3,20 @@
 	app.js ist der Startpunkt jeder Titanium Mobile App
 */
 
-/*
-	Hier wird eine Instanz eines Window-Objektes erstellt.
-	Dabei wird die Hintergrundgfarbe auf weiss gesetzt.
-*/
+// erstellen des Namespace "tfl"
+var tfl = {};
+
+// Laden der Datei ui.js
+Ti.include(
+	'ui.js' // Datei für die UI
+);
 
 // Ti.API.info ist quasi die Logger-Klasse von TM.
 // "warn" und "debug" sind auch verfügbar.
 Ti.API.info('Appstart unter '+Ti.Platform.osname);
 
-var win = Ti.UI.createWindow({
-  backgroundColor:'white'
-});
-
-// Nun noch ein Label erstellen um Text an zu zeigen
-var lbl = Ti.UI.createLabel({
-  text: 'Titanium Flash Light...light up the dark!',
-  height:'auto',
-  width:'auto'
-});
-
-// Das Label wird auf das Window "geklebt"
-win.add(lbl);
-
-// Starte bzw. öffne das Fenster
+// Die erstellt eine Instanz der Methode createApplicationWindow
+// und startet dann das Window-Objekt.
+var win = tfl.ui.createApplicationWindow();
+// start UI things
 win.open();
